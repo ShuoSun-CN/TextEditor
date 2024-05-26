d505540a860afe4b482da00f8bc67c524c8b6c01
Laverne24 <1974996288@qq.com>
2024-05-26 18:54:03 +0800

    修改文件

---
+++ editorvue2/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <MyEditor/>
+  </div>
+</template>
+
+<script>
+  import MyEditor from "@/components/MyEditor.vue";
+export default {
+  name: 'App',
+  components: {
+    MyEditor,
+  }
+}
+</script>
+
+<style>
+
+</style>
