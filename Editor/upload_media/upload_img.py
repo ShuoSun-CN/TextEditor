127dda9a0bbb1c337e14a772e0673b9e8d4207bc
Jalekk <204657592@qq.com>
2024-05-28 16:38:03 +0800

    add upload image back end

--- Editor/upload_media/upload_img.py
+++ Editor/upload_media/upload_img.py
@@ -1,24 +1,41 @@
 from django.http import JsonResponse
 
 from django.views.decorators.csrf import csrf_exempt
+import time
+import numpy as np
 
+def getNewName(file_type):
+    # 前面是file_type+年月日时分秒
+    new_name = time.strftime(file_type+'-%Y%m%d%H%M%S', time.localtime())
+    # 最后是5个随机数字
+    # Python中的numpy库中的random.randint(a, b, n)表示随机生成n个大于等于a，小于b的整数
+    ranlist = np.random.randint(0, 10, 5)
+    for i in ranlist:
+        new_name += str(i)
+    # 返回字符串
+    return new_name
 @csrf_exempt
-def upload_img(req):
+def upload_img(request):
     try:
-        file = req.FILES['specialty_image']
+        if request.method == 'POST' and request.FILES['wangeditor-uploaded-image']:
+            uploaded_file = request.FILES['wangeditor-uploaded-image']
+            file_type=uploaded_file.name.split('.')[-1]
+            new_name=getNewName('IMG')+'.'+file_type
+            with open('media/image/'+new_name,'wb') as ff:
+                for chunk in uploaded_file.chunks():
+                    ff.write(chunk)
         return JsonResponse({
-            "error":0,
+            "errno":0,
             "data":{
-                "url":"http://127.0.0.1:8000/image/1.jpg",
+                "url":"http://127.0.0.1:8000/image/"+new_name,
                 "alt":"",
                 "href":""
             }
-
         })
     except Exception as e:
         print(e)
         return JsonResponse({
-            "error":1,
+            "errno":1,
             "message":"上传失败，请重试~"
         })
 
