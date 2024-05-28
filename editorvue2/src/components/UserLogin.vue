2ee6e5132faec4c7fdf505b7f134fd099419862d
luyuhanazan <1964711271@qq.com>
2024-05-29 00:59:00 +0800

    前端页面修改

--- editorvue2/src/components/UserLogin.vue
+++ editorvue2/src/components/UserLogin.vue
@@ -1,82 +1,150 @@
 <template>
-    <div class="login-container">
-        <el-card class="login-card" :body-style="{backgroundColor: 'transparent'}">
-            <el-form ref="loginForm" :model="loginForm" label-width="80px" class="login-form">
-                <el-form-item label="用户名" prop="user_id">
+  <div class="login">
+    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form">
+      <h3 class="title">富文本编辑器</h3>
+     <el-form-item label="用户名" prop="user_id">
                     <el-input v-model="loginForm.user_id" autocomplete="off" placeholder="请输入用户名"></el-input>
                 </el-form-item>
                 <el-form-item label="密码" prop="password">
                     <el-input type="password" v-model="loginForm.password" autocomplete="off" placeholder="请输入密码"></el-input>
                 </el-form-item>
-                <el-form-item>
-                    <el-button type="primary" @click="submitForm" style="width: 100%;">登录</el-button>
-                    <router-link to="/UserRegister">没有账号？去注册</router-link>
-                </el-form-item>
-                <el-form-item>
-                    <router-link to="/ForgetPassword">忘记密码，去验证</router-link>
-                </el-form-item>
-            </el-form>
-        </el-card>
-    </div>
+
+      <el-checkbox v-model="loginForm.rememberMe" style="margin:0px 0px 25px 0px;">记住密码</el-checkbox>
+       <div style="float: right;" v-if="register">
+          <router-link class="link-type" :to="'/ForgetPassword'">忘记密码</router-link>
+        </div>
+      <el-form-item style="width:100%;">
+        <el-button
+          :loading="loading"
+          size="medium"
+          type="primary"
+          style="width:100%;"
+          @click.native.prevent="handleLogin"
+        >
+          <el-button type="primary" @click="submitForm" style="width: 100%;">登录</el-button>
+
+        </el-button>
+        <div style="float: right;" v-if="register">
+          <router-link class="link-type" :to="'/UserRegister'">立即注册</router-link>
+        </div>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
+
 <script>
-    import { login } from '@/api/UserLogin';
-    export default {
-        data() {
-            return {
-                loginForm: {
-                    user_id: '',
-                    password: ''
-                }
-            };
-        },
-        methods: {
-            submitForm() {
-                this.handleLogin(this.loginForm.user_id, this.loginForm.password);
-            },
-            async handleLogin(user_id, password) {
-                try {
-                    const response = await login(user_id, password);
-                    const data = response;
-                    if (data.code === 0) {
-                        this.$router.push('/MyEditor');
-                    } else {
-                        this.$message.error('用户名或密码错误');
-                    }
-                } catch (error) {
-                    console.error('登录失败:', error);
-                    this.$message.error('登录失败，请稍后重试');
-                }
-            }
-        }
+import { login } from '@/api/UserLogin';
+export default {
+  data() {
+    return {
+      loginForm: {
+        user_id: '',
+        password: ''
+      },
+      captchaEnabled: true,
+      loading: false,
+      codeUrl: '',  // Add your captcha URL here
+      register: true
     };
+  },
+  methods: {
+    submitForm() {
+      this.handleLogin(this.loginForm.user_id, this.loginForm.password);
+    },
+    async handleLogin(user_id, password) {
+      this.loading = true;
+      try {
+        const response = await login(user_id, password);
+        const data = response;
+        if (data.code === 0) {
+          this.$router.push('/MyEditor');
+        } else {
+          this.$message.error('用户名或密码错误');
+        }
+      } catch (error) {
+        console.error('登录失败:', error);
+        this.$message.error('登录失败，请稍后重试');
+      } finally {
+        this.loading = false;
+      }
+    },
+    getCode() {
+      // Logic to refresh captcha
+    }
+  }
+};
 </script>
+
 <style scoped>
-    .login-container {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-        background-image: linear-gradient(-20deg, #e9defa 0%, #fbfcdb 100%);
-    }
+html, body {
+  height: 100%;
+  margin: 0;
+}
 
-    .login-card {
-        width: 400px;
-        padding: 20px;
-        border-radius: 10px;
-        background: transparent;
-        box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-    }
+.login {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-image: linear-gradient(-20deg, #e9defa 0%, #fbfcdb 100%);
+  background-size: cover;
+}
 
-    .login-form {
-        margin-top: 20px;
-    }
+.title {
+  margin: 0px auto 30px auto;
+  text-align: center;
+  color: #707070;
+}
 
-    .el-input__inner {
-        border-radius: 5px;
+.login-form {
+  border-radius: 6px;
+  background: transparent;
+  width: 400px;
+  border: 5px solid white;
+  padding: 25px 25px 5px 25px;
+  .el-input {
+    height: 38px;
+    input {
+      height: 38px;
     }
+  }
+  .input-icon {
+    height: 39px;
+    width: 14px;
+    margin-left: 2px;
+  }
+}
 
-    .el-button {
-        border-radius: 5px;
-    }
+.login-tip {
+  font-size: 13px;
+  text-align: center;
+  color: #bfbfbf;
+}
+
+.login-code {
+  width: 33%;
+  height: 38px;
+  float: right;
+  img {
+    cursor: pointer;
+    vertical-align: middle;
+  }
+}
+
+.el-login-footer {
+  height: 40px;
+  line-height: 40px;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
+  color: #fff;
+  font-family: Arial;
+  font-size: 12px;
+  letter-spacing: 1px;
+}
+
+.login-code-img {
+  height: 38px;
+}
 </style>
--- editorvue2/src/components/UserRegister.vue
+++ editorvue2/src/components/UserRegister.vue
@@ -1,30 +1,79 @@
 <template>
-    <div class="register-container">
-        <el-card class="register-card" :body-style="{backgroundColor: 'transparent'}">
-            <el-form ref="registerForm" :model="registerForm" label-width="80px" class="register-form">
-                <el-form-item label="用户名" prop="user_id">
-                    <el-input v-model="registerForm.user_id" autocomplete="off" placeholder="请输入用户名"></el-input>
-                </el-form-item>
-                <el-form-item label="密码" prop="password">
-                    <el-input type="password" v-model="registerForm.password" autocomplete="off" placeholder="请输入密码"></el-input>
-                </el-form-item>
-                <el-form-item label="邮箱" prop="email">
-                    <el-input v-model="registerForm.email" autocomplete="off" placeholder="请输入邮箱"></el-input>
-                </el-form-item>
-                <el-form-item>
+  <div class="register">
+    <el-form ref="registerForm" :model="registerForm" :rules="registerRules" class="register-form">
+      <h3 class="title">富文本编辑器</h3>
+      <el-form-item prop="user_id">
+        <el-input v-model="registerForm.username" type="text" auto-complete="off" placeholder="用户名">
+          <svg-icon slot="prefix" icon-class="user" class="el-input__icon input-icon" />
+        </el-input>
+      </el-form-item>
+      <el-form-item prop="password">
+        <el-input
+          v-model="registerForm.password"
+          type="password"
+          auto-complete="off"
+          placeholder="密码"
+          @keyup.enter.native="handleRegister"
+        >
+          <svg-icon slot="prefix" icon-class="password" class="el-input__icon input-icon" />
+        </el-input>
+      </el-form-item>
+      <el-form-item prop="confirmPassword">
+        <el-input
+          v-model="registerForm.confirmPassword"
+          type="password"
+          auto-complete="off"
+          placeholder="确认密码"
+          @keyup.enter.native="handleRegister"
+        >
+          <svg-icon slot="prefix" icon-class="password" class="el-input__icon input-icon" />
+        </el-input>
+      </el-form-item>
+      <el-form-item prop="email">
+        <el-input v-model="registerForm.username" type="text" auto-complete="off" placeholder="邮箱">
+          <svg-icon slot="prefix" icon-class="user" class="el-input__icon input-icon" />
+        </el-input>
+      </el-form-item>
+      <el-form-item>
                     <el-button type="primary" @click="sendCode" style="width: 100%;">点击获取邮箱验证码</el-button>
                 </el-form-item>
                 <el-form-item label="验证码" prop="email_code">
                     <el-input v-model="registerForm.email_code" autocomplete="off" placeholder="请输入验证码"></el-input>
                 </el-form-item>
-                <el-form-item>
-                    <el-button type="primary" @click="submitForm" style="width: 100%;">注册</el-button>
-                    <router-link to="/UserLogin">已有账号？去登录</router-link>
-                </el-form-item>
-            </el-form>
-        </el-card>
-    </div>
+
+      <el-form-item prop="code" v-if="captchaEnabled">
+        <el-input
+          v-model="registerForm.code"
+          auto-complete="off"
+          placeholder="验证码"
+          style="width: 63%"
+          @keyup.enter.native="handleRegister"
+        >
+          <svg-icon slot="prefix" icon-class="validCode" class="el-input__icon input-icon" />
+        </el-input>
+        <div class="register-code">
+          <img :src="codeUrl" @click="getCode" class="register-code-img"/>
+        </div>
+      </el-form-item>
+      <el-form-item style="width:100%;">
+        <el-button
+          :loading="loading"
+          size="medium"
+          type="primary"
+          style="width:100%;"
+          @click.native.prevent="handleRegister"
+        >
+          <span v-if="!loading">注 册</span>
+          <span v-else>注 册 中...</span>
+        </el-button>
+        <div style="float: right;">
+          <router-link class="link-type" :to="'/login'">使用已有账户登录</router-link>
+        </div>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
+
 
 <script>
     import { send_fm_code, register } from "@/api/UserLogin";
@@ -74,33 +123,66 @@
         }
     };
 </script>
-
-<style scoped>
-.register-container {
+<style rel="stylesheet/scss" lang="scss">
+.register {
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+  min-height: 100vh; // 保证最小高度为100vh，填满整个屏幕
   background-image: linear-gradient(-20deg, #e9defa 0%, #fbfcdb 100%);
+  background-size: cover;
 }
 
-.register-card {
-  width: 400px;
-  padding: 20px;
-  border-radius: 10px;
-  background: transparent;
-  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+.title {
+  margin: 0px auto 30px auto;
+  text-align: center;
+  color: #707070;
 }
 
 .register-form {
-  margin-top: 20px;
+  border-radius: 6px;
+  background:transparent;
+   border: 5px solid white;
+  width: 400px;
+  padding: 25px 25px 5px 25px;
+  .el-input {
+    height: 38px;
+    input {
+      height: 38px;
+    }
+  }
+  .input-icon {
+    height: 39px;
+    width: 14px;
+    margin-left: 2px;
+  }
 }
-
-.el-input__inner {
-  border-radius: 5px;
+.register-tip {
+  font-size: 13px;
+  text-align: center;
+  color: #bfbfbf;
 }
-
-.el-button {
-  border-radius: 5px;
+.register-code {
+  width: 33%;
+  height: 38px;
+  float: right;
+  img {
+    cursor: pointer;
+    vertical-align: middle;
+  }
+}
+.el-register-footer {
+  height: 40px;
+  line-height: 40px;
+  text-align: center;
+  color: #fff;
+  font-family: Arial;
+  font-size: 12px;
+  letter-spacing: 1px;
+}
+.register-code-img {
+  height: 38px;
 }
 </style>
+
