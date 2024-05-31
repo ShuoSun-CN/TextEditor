bf142b9cad5a661dcc4ea0b1b506a320865052ee
Laverne24 <1974996288@qq.com>
2024-05-31 16:10:08 +0800

    Remove .idea folder

--- editorvue2/src/App.vue
+++ editorvue2/src/App.vue
@@ -5,14 +5,6 @@
 </template>
 
 <script>
-<<<<<<< HEAD
-export default {
-  name: 'App',
-  components: {},
-  created() {
-    // Redirect to UserLogin route when the app is created
-    this.$router.replace('/UserLogin');
-=======
 
   export default {
     name: 'App',
@@ -21,9 +13,7 @@ export default {
       // Redirect to UserLogin route when the app is created
       this.$router.replace('/UserLogin');
     }
->>>>>>> a1b2960133127add8058edfca759688b9fae860e
   }
-}
 </script>
 
 <style>
--- editorvue2/src/components/MyEditor.vue
+++ editorvue2/src/components/MyEditor.vue
@@ -2,18 +2,18 @@
   <div>
     <div style="border: 1px solid #06164d;">
       <Toolbar
-          style="border-bottom: 1px solid rgba(102,117,169,0.75)"
-          :editor="editor"
-          :defaultConfig="toolbarConfig"
-          :mode="mode"
+        style="border-bottom: 1px solid rgba(102,117,169,0.75)"
+        :editor="editor"
+        :defaultConfig="toolbarConfig"
+        :mode="mode"
       />
       <Editor
-          style="height: 500px; overflow-y: hidden;"
-          v-model="html"
-          :defaultConfig="editorConfig"
-          :mode="mode"
-          @onCreated="onCreated"
-          @onChange="onChange"
+        style="height: 500px; overflow-y: hidden;"
+        v-model="html"
+        :defaultConfig="editorConfig"
+        :mode="mode"
+        @onCreated="onCreated"
+        @onChange="onChange"
       />
     </div>
     <span class="wordNumber">{{ TiLength }}/{{ maxChars }}</span>
@@ -26,13 +26,13 @@
 </template>
 
 <script>
-import Vue from 'vue'
 import { Editor, Toolbar } from '@wangeditor/editor-for-vue'
 import registerMenu from "@/utils";
 import axios from "axios";
-export default Vue.extend({
+
+export default {
   name: 'TextEditor',
-  components: {Editor, Toolbar},
+  components: { Editor, Toolbar },
   props: {
     contents: {
       type: String,
@@ -46,10 +46,13 @@ export default Vue.extend({
   data() {
     return {
       editor: null,
-      html: '<p>hello</p>',
+      html: this.contents || '<p>hello</p>',
       toolbarConfig: {
         insertKeys: {
-          keys: ['|', 'myMenuOCR', 'myMenuVideoExtract', 'myMenuAudioExtract', '|', 'myMenuFormatting', '|', 'myMenuPolishing', '|']
+          keys: [
+            '|', 'myMenuOCR', 'myMenuVideoExtract', 'myMenuAudioExtract', '|',
+            'myMenuFormatting', '|', 'myMenuPolishing', '|'
+          ]
         },
         excludeKeys: []
       },
@@ -71,12 +74,15 @@ export default Vue.extend({
         },
         placeholder: '请输入内容...',
         readOnly: false,
+        hoverbarKeys:{
+
+        },
       },
       mode: 'default', // or 'simple'
       TiLength: 0,
       warnShow: false,
-      changedMaxLen: false,
-      maxChars: 0,
+      changedMaxLen: this.changeMaxLen,
+      maxChars: this.changeMaxLen ? 5000 : 1000,
     }
   },
   watch: {
@@ -86,6 +92,7 @@ export default Vue.extend({
           this.editor.setHtml(newV);
         }
       },
+      immediate: true,
     },
     changeMaxLen: {
       handler(newV) {
@@ -94,9 +101,6 @@ export default Vue.extend({
       },
       immediate: true,
     },
-  },
-  created() {
-
   },
   methods: {
     onCreated(editor) {
@@ -111,7 +115,7 @@ export default Vue.extend({
       console.log("原有菜单功能");
       console.log(editor.getAllMenuKeys());
       // 注册自定义菜单功能
-      registerMenu(this.editor,this.toolbarConfig);
+      registerMenu(this.editor, this.toolbarConfig);
       console.log("所有菜单功能");
       console.log(editor.getAllMenuKeys());
 
@@ -159,23 +163,22 @@ export default Vue.extend({
       });
     },
     onChange(editor) {
-      //const text = editor.getText();
       const html = editor.getHtml();
-      const reg = /<[^<>]+>/g;
-      let value = html.replace(reg, '');
-      value = value.replace(/&nbsp;/gi, '');
-      this.TiLength = value.length;
+      const text = editor.getText().replace(/<[^<>]+>/g, '').replace(/&nbsp;/gi, '');
+      console.log("html："+html);
+      console.log("纯文本："+text);
+
+      this.TiLength = text.length;
       this.warnShow = this.changedMaxLen ? this.TiLength > 5000 : this.TiLength > 1000;
-      //this.$emit('getContents', html, text);
+      this.$emit('getContents', html, text);
     },
   },
   beforeDestroy() {
-    const editor = this.editor;
-    if (editor) {
-      editor.destroy();
+    if (this.editor) {
+      this.editor.destroy();
     }
   }
-})
+}
 </script>
 
 <style src="@wangeditor/editor/dist/css/style.css"></style>
