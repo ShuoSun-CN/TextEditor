b0f7ef974d4e0f81f9ce424f58cc3bdd89c71f4e
Laverne24 <1974996288@qq.com>
2024-05-31 16:22:05 +0800

    增加了润色功能

--- editorvue2/src/App.vue
+++ editorvue2/src/App.vue
@@ -5,25 +5,14 @@
 </template>
 
 <script>
-<<<<<<< HEAD
 
-  export default {
-    name: 'App',
-    components: {},
-    created() {
-      // Redirect to UserLogin route when the app is created
-      this.$router.replace('/UserLogin');
-    }
-=======
 export default {
   name: 'App',
   components: {},
   created() {
     // Redirect to UserLogin route when the app is created
     this.$router.replace('/UserLogin');
-
->>>>>>> d5b8336089736c11db28896239be65605a9d4395
-  }
+  }}
 </script>
 
 <style>
