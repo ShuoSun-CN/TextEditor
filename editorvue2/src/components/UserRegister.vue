000b27fc57fefccbb1b9c5f97afa50fd5bbf1a31
luyuhanazan <1964711271@qq.com>
2024-06-01 11:29:32 +0800

    注册登录美化

--- editorvue2/src/components/UserRegister.vue
+++ editorvue2/src/components/UserRegister.vue
@@ -1,35 +1,76 @@
 <template>
   <div class="register">
-    <el-form ref="registerForm" :model="registerForm" :rules="registerRules" class="register-form">
-      <h3 class="title">富文本编辑器</h3>
-      <el-form-item label="用户名" prop="user_id">
-        <el-input v-model="registerForm.user_id" autocomplete="off" placeholder="请输入用户名"></el-input>
-      </el-form-item>
-      <el-form-item label="密码" prop="password">
-        <el-input type="password" v-model="registerForm.password" autocomplete="off" placeholder="请输入密码"></el-input>
-      </el-form-item>
-      <el-form-item label="确认密码" prop="confirmPassword">
-        <el-input type="password" v-model="registerForm.confirmPassword" autocomplete="off" placeholder="再次输入密码"></el-input>
-      </el-form-item>
-      <el-form-item label="邮箱" prop="email">
-        <el-input v-model="registerForm.email" type="text" auto-complete="off" placeholder="请输入正确邮箱号"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" :disabled="!passwordsMatch" @click="sendCode" style="width: 100%;">点击获取邮箱验证码</el-button>
-      </el-form-item>
-      <el-form-item label="验证码" prop="email_code">
-        <el-input v-model="registerForm.email_code" autocomplete="off" placeholder="请输入验证码"></el-input>
-      </el-form-item>
-      <el-form-item style="width:100%;">
-        <el-button :loading="loading" size="medium" type="primary" style="width:100%;" @click.prevent="handleRegister">
-          <span v-if="!loading">注 册</span>
-          <span v-else>注 册 中...</span>
-        </el-button>
-        <div style="float: right;">
-          <router-link class="link-type" :to="'/UserLogin'">使用已有账户登录</router-link>
-        </div>
-      </el-form-item>
-    </el-form>
+    <div class="register-container">
+      <div class="register-left">
+        <el-form ref="registerForm" :model="registerForm" :rules="registerRules" class="register-form">
+          <h3 class="title">文曲星智能编辑器</h3>
+            <div class="compact-form-container">
+              <el-form-item label="用户名" prop="user_id" class="compact-form-item">
+                <el-input v-model="registerForm.user_id"  placeholder="请输入用户名"></el-input>
+              </el-form-item>
+
+              <el-form-item label="邮箱" prop="email" class="compact-form-item">
+                <el-input v-model="registerForm.email" type="text"
+                          placeholder="请输入正确邮箱号"></el-input>
+              </el-form-item>
+            </div>
+
+
+          <div class="button-container">
+            <el-form-item style="width:100%;">
+              <el-button
+                  :loading="loading"
+                  size="medium"
+                  type="primary"
+                  class="register-button"
+                  @click="sendCode"
+              >
+                获取邮箱验证码
+              </el-button>
+            </el-form-item>
+          </div>
+          <div class="compact-form-container">
+            <el-form-item label="密码" prop="password" class="compact-form-item">
+              <el-input type="password" v-model="registerForm.password"
+                        placeholder="请输入密码"></el-input>
+            </el-form-item>
+            <el-form-item label="确认密码" prop="confirmPassword" class="compact-form-item">
+              <el-input type="password" v-model="registerForm.confirmPassword"
+                        placeholder="再次输入密码"></el-input>
+            </el-form-item>
+          </div>
+          <el-form-item label="验证码" prop="email_code">
+            <el-input v-model="registerForm.email_code" autocomplete="off" placeholder="请输入验证码"></el-input>
+          </el-form-item>
+          <div class="button-container">
+            <el-form-item style="width:100%;">
+              <el-button
+                  size="medium"
+                  type="primary"
+                  class="register-button"
+                  @click="handleRegister"
+              >
+                注册
+              </el-button>
+            </el-form-item>
+          </div>
+          <div class="button-container">
+            <el-form-item style="width:100%;">
+              <el-button
+                  :loading="registerloading"
+                  size="medium"
+                  type="primary"
+                  class="forget-password-button"
+                  @click="Login"
+              >
+                使用已有账户登录
+              </el-button>
+            </el-form-item>
+          </div>
+        </el-form>
+      </div>
+      <div class="register-right"></div>
+    </div>
   </div>
 </template>
 
@@ -47,9 +88,8 @@ export default {
         email_code: ''
       },
       loading: false,
-      registerRules:{
-
-      }
+      registerloading: false,
+      registerRules: {}
     };
   },
   computed: {
@@ -58,13 +98,17 @@ export default {
     }
   },
   methods: {
+    async Login() {
+      this.registerloading = true; // 开始注册加载状态
+      this.$router.push('/UserLogin');
+    },
     async sendCode() {
       if (!this.passwordsMatch) {
         this.$message.error('两次输入的密码不一致');
         return;
       }
       try {
-        const response = await send_rm_code({ email: this.registerForm.email });
+        const response = await send_rm_code({email: this.registerForm.email});
         if (response.code === 1) {
           this.$message.success('验证码发送成功，请查收');
         }
@@ -73,7 +117,7 @@ export default {
         this.$message.error('发送验证码失败，请稍后重试');
       }
     },
-     async handleRegister() {
+    async handleRegister() {
       this.loading = true;
       try {
         const response = await register(this.registerForm.user_id, this.registerForm.password, this.registerForm.email, this.registerForm.email_code);
