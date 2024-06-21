2373810e217891fad8b4bbcc026f7c449be7c586
Jalekk <204657592@qq.com>
2024-06-21 10:18:09 +0800

    添加了分享

---
+++ DAO/Shared.py
@@ -0,0 +1,10 @@
+from django.db import models
+class Shared(models.Model):
+    class Meta:
+        db_table='shared'
+    file_id=models.CharField(primary_key=True)
+
+    user_id=models.CharField()
+    expired_time=models.DateTimeField()
+
+    priority=models.IntegerField()
