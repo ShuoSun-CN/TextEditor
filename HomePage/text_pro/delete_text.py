4a04e3ef1074b0c4902feb1e4a1cb7bd2e2c59ff
Jalekk <204657592@qq.com>
2024-06-25 10:13:31 +0800

    增加修改密码

--- HomePage/text_pro/delete_text.py
+++ HomePage/text_pro/delete_text.py
@@ -40,3 +40,38 @@ def delete_own_file(req):
         return JsonResponse({
             "code": 1
         })
+
+
+@csrf_exempt
+def delete_own_file_list(req):
+    try:
+        user_id = verify_session_uid(req)
+        if user_id is None:
+            return JsonResponse({
+                "code": -1
+            })
+        content = req.body
+        content = json.loads(content.decode('UTF-8'))
+        text_id = content['text_id']
+        text_db = Text.objects.filter(file_id=text_id)
+        if not text_db.exists():
+            return JsonResponse({
+                "code": 2,
+                "message": "文件不存在，非法的文件访问！"
+            })
+        if text_db[0].owner == user_id:
+            text_db.delete()
+            return JsonResponse({
+                "code":0,
+            })
+        else:
+            return JsonResponse({
+                "code":3,
+                "message":"当前用户无权删除该文件！"
+            })
+
+    except Exception as e:
+        print(e)
+        return JsonResponse({
+            "code": 1
+        })
