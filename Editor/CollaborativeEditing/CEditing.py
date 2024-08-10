9452ab36af5f61567abf3547636c66922de6fa68
Jalekk <204657592@qq.com>
2024-08-10 17:17:07 +0800

    修改协作显示信息

--- Editor/CollaborativeEditing/CEditing.py
+++ Editor/CollaborativeEditing/CEditing.py
@@ -6,6 +6,7 @@ from urllib.parse import parse_qs
 import threading
 from DAO.Shared import Shared
 from DAO.Text import Text
+from DAO.UserInfo import UserInfo
 from collections import defaultdict
 class ChatConsumer(AsyncWebsocketConsumer):
     active_users = defaultdict(list)
@@ -16,14 +17,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
         # 获取特定参数
         self.text_id = query_params.get('text_id', [None])[0]
         session_id = query_params.get('session_id', [None])[0]
-        user_id=verify_only_session_uid(session_id)
-        owner=Text.objects.filter(owner=user_id,file_id=self.text_id)
-        shared=Shared.objects.filter(user_id=user_id,file_id=self.text_id)
+        self.user_id=verify_only_session_uid(session_id)
+        owner=Text.objects.filter(owner=self.user_id,file_id=self.text_id)
+        shared=Shared.objects.filter(user_id=self.user_id,file_id=self.text_id)
         if not owner.exists() and not shared.exists():
             await self.close()
             return
         # 将用户添加到房间组的活跃用户列表中
-        self.active_users[self.text_id].append(user_id)
+        self.active_users[self.text_id].append(self.user_id)
         await self.channel_layer.group_add(
             self.text_id,
             self.channel_name
@@ -31,7 +32,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
 
         await self.accept()
-        print("1111111111")
         await self.channel_layer.group_send(
             self.text_id,
             {
@@ -66,6 +66,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     # 接收来自 WebSocket 的消息
     async def receive(self, text_data):
+
         text_data_json = json.loads(text_data)
         message = text_data_json['text']
         print("websocket receive message: ",message)
@@ -81,16 +82,27 @@ class ChatConsumer(AsyncWebsocketConsumer):
     # 接收来自房间组的消息
     async def update_text(self, event):
         message = event['text']
-
         # 发送消息到 WebSocket
         await self.send(text_data=json.dumps({
+            'action':"update_text",
             'text': message
-        }))
-    # 接收来自房间组的用户列表更新消息
+        })
+        )
+
+
+
     async def user_list_update(self, event):
         users = event['users']
-        print(users)
+        users_info=[]
+        for i in users:
+            user_info=UserInfo.objects.filter(user_id=i)[0]
+            temp_info={}
+            temp_info['avatar']=user_info.user_avatar
+            temp_info['user_name']=user_info.user_name
+            users_info.append(temp_info)
         # 发送用户列表到 WebSocket
+        print(users_info)
         await self.send(text_data=json.dumps({
-            'users': users
+            'action':"update_users",
+            'users': json.dumps(users_info)
         }))
